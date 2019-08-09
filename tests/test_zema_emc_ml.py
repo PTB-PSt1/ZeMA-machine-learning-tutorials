0a9833ed988fd2dd7f657f3f03ca71c339dba9d2
Bjoern Ludwig <bjoern.ludwig@ptb.de>
2019-08-09 08:46:48 +0200

    Insert first draft of tests

---
+++ tests/test_zema_emc_ml.py
@@ -0,0 +1,31 @@
+import os
+import subprocess
+import tempfile
+
+import nbformat
+
+def test_tutorial_1():
+
+    def _notebook_run(path):
+        """Execute a notebook via nbconvert and collect output.
+           :returns (parsed nb object, execution errors)
+        """
+        dirname, __ = os.path.split(path)
+        os.chdir(dirname)
+        with tempfile.NamedTemporaryFile(suffix=".ipynb") as fout:
+            args = ["nbconvert", "--to", "notebook", "--execute",
+                    "--ExecutePreprocessor.timeout=60",
+                    "--output", fout.name, path]
+            subprocess.check_call(args)
+
+            fout.seek(0)
+            nb = nbformat.read(fout, nbformat.current_nbformat)
+
+        errors = [output for cell in nb.cells if "outputs" in cell
+                  for output in cell["outputs"] \
+                  if output.output_type == "error"]
+
+        return nb, errors
+
+    path = "0_Data_Import_and_Visualisation.ipynb"
+    _notebook_run(path)
